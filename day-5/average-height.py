6509d3143e167ad58ea304aa60ca9c4ff70e3ea8
Benjamin W <56703794+Snoower@users.noreply.github.com>
2023-08-15 18:09:51 -0400

    Create average-height.py

---
+++ day-5/average-height.py
@@ -0,0 +1,9 @@
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+total_height = 0
+for height in student_heights:
+  total_height += height
+avg = (total_height/(n+1))
+print(round(avg))
