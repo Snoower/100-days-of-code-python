149649068ccf3e68ed5de4e8aa1f0a9bde7ed4af
Benjamin W <56703794+Snoower@users.noreply.github.com>
2023-08-17 19:20:19 -0400

    Update hangman-final.py

--- day-7/hangman-final.py
+++ day-7/hangman-final.py
@@ -17,10 +17,12 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+  
     clear()
   
     if guess in display:
       print(f"You already guessed {guess}.")
+      
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
