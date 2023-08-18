00504ca83739e78ec40bc9829cc469251c9bf086
Benjamin W <56703794+Snoower@users.noreply.github.com>
2023-08-18 15:40:38 -0400

    Create greet.py

---
+++ day-8/greet.py
@@ -0,0 +1,5 @@
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"Are you from {location}?")
+greet_with("Tomato", "the garden") #positional argument
+greet_with(name="Banana", location="the tree") #keyword argument
