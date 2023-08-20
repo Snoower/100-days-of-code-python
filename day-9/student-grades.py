5d5fd8a42eb460dd3e3658339f9f5364ef56913e
Benjamin W <56703794+Snoower@users.noreply.github.com>
2023-08-19 23:03:48 -0400

    Create student-grades.py

---
+++ day-9/student-grades.py
@@ -0,0 +1,27 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+student_grades = {}
+
+for student in student_scores:
+  score = student_scores[student]
+  if score >= 91:
+      student_grades[student] = "Outstanding"
+  elif score < 91 and score >= 81:
+      student_grades[student] = "Exceeds Expectations"
+  elif score < 80 and score >= 71:
+      student_grades[student] = "Acceptable"
+  else:
+      student_grades[student] = "Fail"
+        
+print(student_grades)
+
+
+
+
+
