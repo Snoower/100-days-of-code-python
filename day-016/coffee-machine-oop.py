05c0d5b6e7fa316bc0cfeef139bf295e3811f7ab
Benjamin W <56703794+Snoower@users.noreply.github.com>
2023-08-30 13:36:22 -0400

    Update coffee-machine-oop.py

--- day-016/coffee-machine-oop.py
+++ day-016/coffee-machine-oop.py
@@ -1,4 +1,4 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
@@ -21,5 +21,3 @@ while is_on:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
             coffee_maker.make_coffee(drink)
-
-
