d9edb9ee4a52e934b829d1d04cf06aa6afd7e05d
Benjamin W <56703794+Snoower@users.noreply.github.com>
2023-09-26 19:51:09 -0400

    Update main.py

--- day-021/main.py
+++ day-021/main.py
@@ -1,8 +1,8 @@
-# Day 21:
-# Detect collision with food
-# Create a scoreboard
-# Detect collision with wall
-# Detect collision with tail
+# day 21:
+# detect collision with food
+# create a scoreboard
+# detect collision with wall
+# detect collision with tail
 
 from turtle import Screen
 from snake import Snake
@@ -33,18 +33,18 @@ while game_is_on:
 
     snake.move()
 
-    # Detect collision with food
+    # detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    # Detect collision with wall
+    # detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
 
-    # Detect collision with tail
+    # detect collision with tail
     for segment in snake.all_segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
